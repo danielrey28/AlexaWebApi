12ff7d3656e36b72ee86bc317e488b815b6edbbd
Daniel Reyes <Daniel.reyes@namely.com>
2016-09-14 20:30:06 -0400

    first commit

---
+++ AlexaSkill.Data/Requests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlexaSkill.Data
+{
+    public class Requests
+    {
+        public Request Create(Request request)
+        {
+            using (var db = new azurealexaEntities())
+            {
+                var member = db.Members.FirstOrDefault(m => m.AlexaUserId == request.UserId);
+
+                if (member == null)
+                {
+                    request.Member = new Member() {AlexaUserId = request.UserId, CreatedDate = DateTime.UtcNow, LastRequestDate = DateTime.UtcNow, RequestCount = 1};
+                    db.Requests.Add(request);
+                }
+                else
+                {
+                    member.Requests.Add(request);
+                }
+
+                db.SaveChanges();
+            }
+            return request;
+        }
+    }
+}
---
+++ AlexaSkill/Controllers/AlexaController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AlexaSkill.Data;
+
+namespace AlexaSkill.Controllers
+{
+    public class AlexaController : ApiController
+    {
+        [HttpPost, Route("api/alexa/demo")]
+        public dynamic Pluralsight(AlexaRequest alexaRequest)
+        {
+
+            try
+            {
+                var request = new Requests().Create(new Data.Request
+                {
+                    MemberId = alexaRequest.Session.Attributes.MemberId,
+                    Timestamp = alexaRequest.Request.Timestamp,
+                    Intent = (alexaRequest.Request.Intent == null) ? "" : alexaRequest.Request.Intent.Name,
+                    AppId = alexaRequest.Session.Application.ApplicationId,
+                    RequestId = alexaRequest.Request.RequestId,
+                    SessionId = alexaRequest.Session.SessionId,
+                    UserId = alexaRequest.Session.User.UserId,
+                    IsNew = alexaRequest.Session.New,
+                    Version = alexaRequest.Version,
+                    Type = alexaRequest.Request.Type,
+                    Reason = "",
+                    DateCreated = DateTime.UtcNow,
+                    Slots = ""
+
+                });
+
+                var response = new AlexaResponse("Hello world! Would you like for me to say it again? Yes or no?")
+                {
+                    Session = {MemberId = request.MemberId}
+                };
+                response.Response.Card.Title = "Pluralsight";
+                response.Response.Card.Content = "Hello\ncruel world!";
+                response.Response.Reprompt.OutputSpeech.Text = "Say it again? Yes or no?";
+                response.Response.ShouldEndSession = false;
+
+                if (request.Intent == "AMAZON.NoIntent")
+                {
+                    response.Response.OutputSpeech.Text = "Ok, have a nice day.";
+                    response.Response.ShouldEndSession = true;
+                }
+
+                return response;
+                
+            }
+            catch (EntityCommandExecutionException ex)
+            {
+                return new { message = ex.Message, exceptiontype = ex.GetType(), request = alexaRequest, inner = ex.InnerException };
+            }
+            catch (DbEntityValidationException ex)
+            {
+                return new { message = ex.Message, exceptiontype = ex.GetType(), request = alexaRequest, validation = ex.EntityValidationErrors};
+            }
+            catch (Exception ex)
+            {
+                return new {message = ex.Message, exceptiontype = ex.GetType(), exceptionsource = ex.Data};
+            }
+        }
+    }
+
+}
