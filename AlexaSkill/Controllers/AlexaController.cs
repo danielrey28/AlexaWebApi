60df897104ace8d8039fc255e50d091288916f85
Daniel Reyes <Daniel.reyes@namely.com>
2016-09-14 21:43:11 -0400

    Intent Routing

--- AlexaSkill/Controllers/AlexaController.cs
+++ AlexaSkill/Controllers/AlexaController.cs
@@ -5,6 +5,7 @@ using System.Data.Entity.Validation;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Web.Http;
 using AlexaSkill.Data;
 
@@ -36,21 +37,22 @@ namespace AlexaSkill.Controllers
 
                 });
 
-                var response = new AlexaResponse("Hello world! Would you like for me to say it again? Yes or no?")
-                {
-                    Session = {MemberId = request.MemberId}
-                };
-                response.Response.Card.Title = "Pluralsight";
-                response.Response.Card.Content = "Hello\ncruel world!";
-                response.Response.Reprompt.OutputSpeech.Text = "Say it again? Yes or no?";
-                response.Response.ShouldEndSession = false;
+                AlexaResponse response = null;
 
-                if (request.Intent == "AMAZON.NoIntent")
+                switch (request.Type)
                 {
-                    response.Response.OutputSpeech.Text = "Ok, have a nice day.";
-                    response.Response.ShouldEndSession = true;
+                    case "LaunchRequest":
+                        response = LaunchRequestHandler(request);
+                        break;
+                    case "IntentRequest":
+                        response = IntentRequestHandler(request);
+                        break;
+                    case "SessionEndedRequest":
+                        response = SessionEndedRequestHandler(request);
+                        break;
                 }
 
+                
                 return response;
                 
             }
@@ -67,6 +69,85 @@ namespace AlexaSkill.Controllers
                 return new {message = ex.Message, exceptiontype = ex.GetType(), exceptionsource = ex.Data};
             }
         }
+
+        private AlexaResponse LaunchRequestHandler(Request request)
+        {
+            var response = new AlexaResponse("Welcome to Pluralsight. What would you like to hear? Top courses or new courses?")
+            {
+                Session = { MemberId = request.MemberId }
+            };
+            response.Response.Card.Title = "Pluralsight";
+            response.Response.Card.Content = "Hello\ncruel world!";
+            response.Response.Reprompt.OutputSpeech.Text = "Please pick one, Top courses or new courses?";
+            response.Response.ShouldEndSession = false;
+
+            if (request.Intent == "AMAZON.NoIntent")
+            {
+                response.Response.OutputSpeech.Text = "Ok, have a nice day.";
+                response.Response.ShouldEndSession = true;
+            }
+
+            return response;
+        }
+
+        private AlexaResponse IntentRequestHandler(Request request)
+        {
+            AlexaResponse response = null;
+
+            switch (request.Intent)
+            {
+                case "NewCourseIntent":
+                    response = NewCoursesIntentHndler(request);
+                    break;
+                case "TopCoursesIntent":
+                    response = TopCoursesIntentHandler(request);
+                    break;
+            }
+        }
+
+        private AlexaResponse NewCoursesIntentHndler(Request request)
+        {
+            var output = new StringBuilder("Here are the latest courses. ");
+
+            using (var db = new azurealexaEntities())
+            {
+                db.Courses.Take(10).OrderByDescending(c=>c.DateCreated).ToList().ForEach(c=> output.AppendFormat("{0} by {1}. ", c.Title, c.Author));
+            }
+            return new AlexaResponse(output.ToString());
+        }
+
+        private AlexaResponse TopCoursesIntentHandler(Request request)
+        {
+            int limit = 10;
+            if (request.SlotsList.Any())
+            {
+                int maxLimit = 10;
+                var limitValue = request.SlotsList.FirstOrDefault(s => s.Key == "Limit").Value;
+
+                if (!string.IsNullOrWhiteSpace(limitValue) && int.TryParse(limitValue, out limit) && !(limit >= 1 &&
+                    limit <= maxLimit))
+                {
+                    limit = maxLimit;
+                }
+            }
+            var output = new StringBuilder("");
+            output.AppendFormat("Here are the top {0} courses. ", limit);
+
+            using (var db = new azurealexaEntities())
+            {
+                db.Courses.Take(limit).OrderByDescending(c => c.Votes).ToList().ForEach(c => output.AppendFormat("{0} by {1}. ", c.Title, c.Author));
+            }
+            return new AlexaResponse(output.ToString());
+        }
+
+        private AlexaResponse SessionEndedRequestHandler(Request request)
+        {
+            return null;
+        }
+
+        
+
+       
     }
 
 }
